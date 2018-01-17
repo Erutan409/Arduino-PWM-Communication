50645cbc2c2592940bc5c053c3029053f7238f2b
Michael Keyser <Erutan409@users.noreply.github.com>
2018-01-16 22:41:36 -0500

    State of work.

--- PWMC.h
+++ PWMC.h
@@ -3,12 +3,12 @@
 
 //#include <functional-vlpp.h>
 
-#include <PWMC_Types.h>
-#include <PWMC_Enums.h>
-#include <PWMC_Byte.h>
-#include <PWMC_TimeTracking.h>
-#include <PWMC_Handshake.h>
-#include <PWMC_Communication.h>
+#include "PWMC_Types.h"
+#include "PWMC_Enums.h""
+#include "PWMC_Byte.h"
+#include "PWMC_TimeTracking.h"
+#include "PWMC_Handshake.h"
+#include "PWMC_Communication.h"
 
 using PWMC_Communication = PWMC::Communication;
 
